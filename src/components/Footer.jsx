a95fe046f6e5d7724f24e2280803b06dd6ae0cee
Murodali0707 <murodalisiddiqov83@gmail.com>
2024-12-26 18:35:51 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="py-5 border-t text-center">
+      Powered by{" "}
+      <a
+        className="underline"
+        href="https://github.com/Murodali0707"
+        target="_blank"
+      >
+        Murodali
+      </a>
+    </footer>
+  );
+}
